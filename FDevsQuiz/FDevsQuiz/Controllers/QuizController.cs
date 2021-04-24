4c5301b1f7e2abd4d319df1919c5b97268d3cd23
wriqie <henrique5121@outlook.com>
2021-04-24 17:51:17 -0300

    Atividade 1
    Arquivos Atividade 1 Api

---
+++ FDevsQuiz/FDevsQuiz/Controllers/QuizController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FDevsQuiz.Controllers
+{
+    [Controller]
+    [Route("quizzes")]
+    public class QuizController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<dynamic> Quizzes()
+        {
+            return Ok(new 
+            { 
+                CodigoUsuario = 1,
+                CodigoQuiz = 1,
+                NomeQuiz = "Estrutura Banco de Dados",
+                PontuacaoUsuario = 80
+            });
+        }
+    }
+}
---
+++ FDevsQuiz/FDevsQuiz/Controllers/UsuariosController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FDevsQuiz.Controllers
+{
+    [Controller]
+    [Route("dificuldades")]
+    public class DificuldadesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<dynamic> Dificuldades()
+        {
+            return Ok(new 
+            { 
+                CodigoDificuldade = 1,
+                Nome = "Facil",
+                QuantidadeDeQuizzes = 1
+            });
+        }
+    }
+}
